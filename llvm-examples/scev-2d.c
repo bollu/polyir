540dccf0804dd52522b8dda1d3c9496841d5e890
Siddharth Bhat <siddu.druid@gmail.com>
2018-06-05 00:22:33 +0530

    add examples of SCEV to analyze

---
+++ llvm-examples/scev-2d.c
@@ -0,0 +1,9 @@
+const int N = 10;
+void foo(int A[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            const int val = i + j;
+            A[i][j] = val;
+        }
+    }
+}
