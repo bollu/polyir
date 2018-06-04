bff0e3509582c758370e3e9d4d813c2bf5bed1da
Siddharth Bhat <siddu.druid@gmail.com>
2018-06-05 00:33:32 +0530

    Make loop induction variables differ by some delta to be clearer to see what is going on.

--- llvm-examples/scev-2d.c
+++ llvm-examples/scev-2d.c
@@ -1,6 +1,6 @@
 const int N = 10;
 void foo(int A[N][N]) {
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i+=10) {
         for(int j = 0; j < N; j++) {
             const int val = i + j;
             A[i][j] = val;
